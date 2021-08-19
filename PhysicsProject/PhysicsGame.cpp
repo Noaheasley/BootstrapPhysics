00020c06986150acd30b12be5c816640d7d60858
Noah <Noaheasley408@gmail.com>
2021-08-19 12:19:59 -0500

    created rigidBody and Sphere

--- PhysicsProject/PhysicsGame.cpp
+++ PhysicsProject/PhysicsGame.cpp
@@ -1,4 +1,5 @@
 #include "PhysicsGame.h"
+#include "Sphere.h"
 #include "Input.h"
 #include "Font.h"
 #include "Gizmos.h"
@@ -10,11 +11,15 @@ bool PhysicsGame::startup()
 
 	m_renderer = new aie::Renderer2D();
 	setBackgroundColour(0.1f, 0.1f, 0.1f, 0.1f);
-
+	
+	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+	
 	m_scene = new PhysicsScene();
 	m_scene->setTimeStep(0.01f);
+	m_scene->setGravity(glm::vec2{ 0.0f, -25.0f });
 
-	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
+	Sphere* ball = new Sphere(glm::vec2(), glm::vec2(), 1, 10, glm::vec4(0.8f, 0.2f, 0.2f, 1.0f));
+	m_scene->addActor(ball);
 
 	return true;
 }
