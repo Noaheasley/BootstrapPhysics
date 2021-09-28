bc99c8fb26fa8dda8df95fa36606f1dec8b08696
Noah <Noaheasley408@gmail.com>
2021-09-28 12:22:20 -0500

    added opengl

--- GraphicsProject/main.cpp
+++ GraphicsProject/main.cpp
@@ -1,10 +1,32 @@
 #include "gl_core_4_4.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
 
 int main() {
-	if (!glfwInit()) {
+	if (glfwInit() == GLFW_FALSE) {
 		return -1;
 	}
 
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Test Window", nullptr, nullptr);
+	if (!window) {
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	//Load OpenGl
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		return -3;
+	}
+	int majorVersion = ogl_GetMajorVersion();
+	int minorVersion = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", majorVersion, minorVersion);
+
+	//cleanup and exit
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
