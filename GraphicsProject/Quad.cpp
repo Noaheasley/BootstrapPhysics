a3ae07634d9d11fcce73b18c3fa4a73a5e69e1f2
Noah <Noaheasley408@gmail.com>
2021-10-12 10:27:15 -0500

    added Earth texture onto plane

--- GraphicsProject/Quad.cpp
+++ GraphicsProject/Quad.cpp
@@ -34,7 +34,7 @@ Quad::Vertex* Quad::generateVertices(unsigned int& vertexCount, unsigned int& tr
 	vertices[1].texCoord = { 1.0f, 1.0f };
 	vertices[2].texCoord = { 0.0f, 0.0f };
 	//triangle 1
-	vertices[3].texCoord = { 1.0f, 0.0f };
+	vertices[3].texCoord = { 1.0f, 1.0f };
 	vertices[4].texCoord = { 0.0f, 0.0f };
 	vertices[5].texCoord = { 1.0f, 0.0f };
 
